2e19494eb419785828b259bba0b3ebeb72fef4c3
stiglidu <duwh20@mails.tsinghua.edu.cn>
2023-09-12 01:18:47 -0400

    video sort

--- envs/tdw_mat/utils/img_to_video.py
+++ envs/tdw_mat/utils/img_to_video.py
@@ -3,7 +3,7 @@ import os
 
 def images_to_video(image_folder, video_name, fps):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png") and not img.endswith("seg.png") and not img.endswith("depth.png") and not img.endswith("map.png") and not img.endswith("filter.png")]
-    images.sort()
+    images.sort(key = lambda x: int(x.split('.')[0]))
 
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
