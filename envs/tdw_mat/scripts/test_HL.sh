03e1f74a46d05a0cdc04fdd27c665f4a7e95fb68
stiglidu <duwh20@mails.tsinghua.edu.cn>
2023-09-20 22:38:14 -0400

    run config in winne

--- envs/tdw_mat/scripts/test_HL.sh
+++ envs/tdw_mat/scripts/test_HL.sh
@@ -15,7 +15,6 @@ python3 tdw-gym/challenge.py \
 --eval_episodes 0 11 17 18 1 2 3 21 22 23 4 5 6 7 8 9 10 12 13 14 15 16 19 20 \
 --debug \
 --screen_size 256 \
---no_save_img \
---no_gt_mask
+--no_save_img
 
 pkill -f -9 "port $port"
